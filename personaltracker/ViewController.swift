7025140e129c250d0c6840b4a08c66d9c6bc1d4e
Kurniadi <krniadi@hotmail.com>
2022-07-10 08:09:02 +0700

    Initial Commit

---
+++ personaltracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  personaltracker
+//
+//  Created by kur niadi  on 10/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
