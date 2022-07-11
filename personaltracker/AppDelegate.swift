683d9ed7b7667410a5d26ef3b47eba85ab6d465a
Kurniadi <krniadi@hotmail.com>
2022-07-11 20:54:25 +0700

    connecting welcome page with main tab bar

--- personaltracker/AppDelegate.swift
+++ personaltracker/AppDelegate.swift
@@ -34,3 +34,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+extension UIApplication {
+    var mainWindow: UIWindow? {
+        return UIApplication.shared.connectedScenes
+            .filter { $0.activationState == .foregroundActive }
+            .first(where: { $0 is UIWindowScene })
+            .flatMap({ $0 as? UIWindowScene })?.windows
+            .first(where: \.isKeyWindow)
+    }
+}
--- personaltracker/view/WelcomPageViewController.swift
+++ personaltracker/view/WelcomPageViewController.swift
@@ -28,24 +28,36 @@ class WelcomPageViewController: UIViewController {
         
         // Do any additional setup after loading the view.
         setupSignal()
-        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         viewModel.viewLoad()
     }
     
     private func setupSignal() {
         nameTextField.rx.controlEvent([.editingChanged])
-            .asObservable().subscribe({ [unowned self] _ in
-                self.viewModel.onTextChange(text: nameTextField.nonNilText)
+            .asObservable()
+            .observe(on: MainScheduler.asyncInstance)
+            .subscribe({ [weak self] _ in
+                guard let `self` = self else { return }
+                self.viewModel.onTextChange(text: self.nameTextField.nonNilText)
             }).disposed(by: disposeBag)
         
-        goButton.rx.tap.subscribe { [unowned self] _ in
+        goButton.rx.tap.subscribe { [weak self] _ in
+            guard let `self` = self else { return }
             self.viewModel.goTapped(name: self.nameTextField.nonNilText)
         }.disposed(by: disposeBag)
         
         viewModel.event.emit { [unowned self] event in
             switch(event) {
             case .goToMainPage:
-                //
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let vc = storyboard.instantiateViewController(withIdentifier: "MainTabBar") as? UITabBarController
+                UIApplication.shared.mainWindow?.rootViewController = vc
+                
+                UIApplication.shared.mainWindow?.makeKeyAndVisible()
+                
                 break
             case .enableGo(let isEnabled):
                 self.goButton.alpha = isEnabled ? 1.0 : 0.3
