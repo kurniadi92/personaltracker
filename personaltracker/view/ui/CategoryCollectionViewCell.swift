6032d2755bedb74ae5838b421418a4ee69e0f797
Kurniadi <krniadi@hotmail.com>
2022-07-11 21:40:45 +0700

    create ui for add expense or income

---
+++ personaltracker/view/ui/CategoryCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryCollectionViewCell.swift
+//  personaltracker
+//
+//  Created by kur niadi  on 11/07/22.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet var bgView: UIView!
+    @IBOutlet var categoryTitleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        bgView.layer.cornerRadius = 12
+        bgView.layer.borderColor = UIColor.gray.cgColor
+        bgView.layer.borderWidth =  0.3
+    }
+
+}
